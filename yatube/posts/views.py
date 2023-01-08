446114b7d00b8d37f50091316a246aa3fd48031c
Даниил Федотов <pustovdanil@bk.ru>
2023-01-08 20:25:55 +0500

    Добавлены urls и views-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница новейшего веб-приложения!')
+
+
+def group_posts(request, any_slug):
+    return HttpResponse(f'Вы ввели в адресную строку что-то странное: {any_slug}')
